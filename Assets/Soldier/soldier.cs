a2e9760b62097986d4a103883f458b7e8693af71
Léo Gigant <rowix4@gmail.com>
2018-12-19 21:51:08 +0100

    new map great size

---
+++ Assets/Soldier/soldier.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soldier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animation idle = GetComponent<Animation>();
+        idle.Play("Rifle Idle");
+	}
+}

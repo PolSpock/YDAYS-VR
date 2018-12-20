5091d6e660c06caad144001d8d2ba2005e493f78
Léo Gigant <rowix4@gmail.com>
2018-12-20 15:32:02 +0100

    add AIHelico for random move

---
+++ Assets/Scenes/Attack Helicopter2/AIHelico.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIHelico : MonoBehaviour {
+
+    private RaycastHit Hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * 15 * Time.deltaTime);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit, 2))
+        {
+            transform.Rotate(Vector3.up * Random.Range(50, 200));
+        }
+	}
+}

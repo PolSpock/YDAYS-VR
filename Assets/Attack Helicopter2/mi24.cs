fb4a4c32cbeac4b78c306218e5c11f7fddc87bbb
arthur <arthur.genthial@ynov.com>
2018-12-20 13:48:19 +0100

    holoucouptaire de combat

---
+++ Assets/Attack Helicopter2/mi24.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mi24 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animation animations = GetComponent<Animation>();
+        animations.Play("Roatation+ Wheels");
+	}
+}
